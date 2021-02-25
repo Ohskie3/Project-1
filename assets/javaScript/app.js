2ad0dd37ededa35cee2c4f390773d0f617120c8e
Kyle Ohlwiler <kohlwiler@yahoo.com>
2021-02-24 19:44:28 -0800

    added quote API to home page

--- assets/javaScript/app.js
+++ assets/javaScript/app.js
@@ -24,6 +24,18 @@ document.addEventListener('click', event => {
   })
 })
 
+axios.get(`https://favqs.com/api/qotd`)
+  .then(res => {
+    console.log(res.data)
+    let quote = res.data
+    document.getElementById('quotes').innerHTML = `
+            <p>${quote.quote.body}</p>
+            <p> - ${quote.quote.author}</p>
+          `
+
+  })
+  .catch(err => console.error(err))
+
 // Giphy Logic: 
 
 // document.getElementById('ingredients').innerHTML = `
